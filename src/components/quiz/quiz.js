15f62f62445a280512e7bc391519a09b38301506
Harsh Vardhan Sharma <38874238+legend7bot@users.noreply.github.com>
2022-08-06 02:38:57 +0530

    allWorking

--- src/components/quiz/quiz.js
+++ src/components/quiz/quiz.js
@@ -6,9 +6,24 @@ function Quiz(props) {
 
     const [selectedAnswer, setSelectedAnswer] = useState(['', '', '', '', '']);
     const [correctAnswers, setCorrectAnswers] = useState(['', '', '', '', '']);
+    const [readyForNew, setReadyForNew] = useState(false);
+
+
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+
+            // Generate random number
+            var j = Math.floor(Math.random() * (i + 1));
+
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        return array;
+    }
 
     useEffect(() => {
-        console.log(props.questions)
         for (let i = 0; i < props.questions.length; i++) {
             setCorrectAnswers(prevAnswer => {
                 prevAnswer[i] = props.questions[i].correct_answer;
@@ -22,10 +37,10 @@ function Quiz(props) {
     const quizQuestions = props.questions.map((question, index) => {
 
         const answerArray = question.incorrect_answers.concat(question.correct_answer);
-
-        const options = answerArray.map((answer) => {
+        const shuffledAnswers = shuffleArray(answerArray);
+        const options = shuffledAnswers.map((answer) => {
             return (
-                <div onClick={() => selectAnswer(index, answer)} className="quiz-option">
+                <div ansvalue={answer} onClick={(event) => selectAnswer(event, index, answer)} className={`quiz-option que${index}`}>
                     <span>{answer}</span>
                 </div>
             )
@@ -44,21 +59,48 @@ function Quiz(props) {
     function checkAnswer() {
         console.log(`Selected: ${selectedAnswer}`)
         console.log(`Correct: ${correctAnswers}`)
+
+        //possible-breakpoint
+        for (let i = 0; i < props.questions.length; i++) {
+            const a = document.getElementsByClassName(`que${i} selected`)
+            const b = document.getElementsByClassName(`que${i}`)
+            if (correctAnswers[i] === selectedAnswer[i]) {
+                a[0].classList.add('correct')
+            }
+            else {
+                a[0].classList.add('incorrect')
+            }
+            for (let j = 0; j < b.length; j++) {
+                const c = b[j].getAttribute('ansvalue')
+                if (correctAnswers[i] === c) {
+                    b[j].classList.add('correct')
+                }
+            }
+        }
+
         if (selectedAnswer.every((answer, index) => answer === correctAnswers[index])) {
             props.setWin(true);
+            setReadyForNew(true);
+            console.log('You win!')
         }
         else {
-            console.log('You lost!')
+            setReadyForNew(true);
+            console.log('You lose!')
         }
 
     }
 
 
-    function selectAnswer(index, answer) {
-        console.log(index)
+    function selectAnswer(event, index, answer) {
+        const selected = document.getElementsByClassName(`que${index}`)
+        for (let i = 0; i < selected.length; i++) {
+            selected[i].classList.remove('selected');
+        }
+        event.currentTarget.classList.add('selected')
         setSelectedAnswer(prevAnswer => {
             const ayooo = prevAnswer;
             ayooo[index] = answer;
+            console.log(`reee ${ayooo}`)
             return ayooo;
         });
     }
@@ -71,7 +113,7 @@ function Quiz(props) {
         <div className='quiz-container'>
             <h1>Quizzie</h1>
             {quizQuestions}
-            {props.win ?
+            {readyForNew ?
                 <button className='ansChecker' onClick={newQuiz}>New Quiz</button> :
                 <button className='ansChecker' onClick={checkAnswer}>Check Answer</button>
             }
